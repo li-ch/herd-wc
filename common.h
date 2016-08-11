76d89b18eb722c27f86f77f918f4ecca0dba9c17
li <crischenli@gmail.com>
2016-08-11 17:26:45 +0800

    div by 0

--- common.h
+++ common.h
@@ -39,7 +39,7 @@
 #endif
 
 #define NUM_CLIENTS 1			// Number of client processes
-#define NUM_SERVERS 1			// Number of server processes
+#define NUM_SERVERS 2			// Number of server processes
 
 #define Q_DEPTH 1024			// Size of all created queues
 #define S_DEPTH 512
